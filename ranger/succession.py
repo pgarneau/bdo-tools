1155791e2ec24230f39ba263936044b41e724618
Nightfall-WoW <46864578+Nightfall-WoW@users.noreply.github.com>
2025-03-18 20:04:16 +0100

    hello3

--- ranger/succession.py
+++ ranger/succession.py
@@ -12,12 +12,15 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 crit_buff = Spell(Vision('crit', 0.98), None)
 ap_buff = Spell(Vision('ap', 0.98), None)
 shai_buff = Spell(Vision('shai_speed', 0.98), None)
+e_buff_icon = Spell(Vision('e_buff_icon', 0.98), None)
 
 def get_attack_speed():
     speed = 1
     buffs = wincap.get_buffs()
     if ap_buff.ready(buffs):
         speed = speed + 0.15
+    if e_buff_icon.ready(buffs):
+        speed = speed + 0.2
     # elif cast_speed_buff.ready(buffs):
     #     speed = speed + 0.1
     if shai_buff.ready(buffs):
@@ -35,20 +38,20 @@ flow_bypassing_wind = Spell(Vision('flow_bypassing_wind'), Bind('shift','right')
 
 # Succession
 prime_regeneration = Spell(Vision('prime_regeneration'), Bind('s+c', None), 0.75, 7, get_attack_speed)
-prime_natures_tremble = Spell(Vision('prime_natures_tremble'), Bind('w+c', None), 0.85, 7, get_attack_speed)
+prime_natures_tremble = Spell(Vision('prime_natures_tremble'), Bind('w+c', None), 0.75, 7, get_attack_speed)
 prime_penetrating_wind = Spell(Vision('prime_penetrating_wind'), Bind('shift', 'right'), 0.09, 7, get_attack_speed)
 prime_razor_wind = Spell(Vision('prime_razor_wind'), Bind('e', 'left', hold_bind), 2.3, 8, get_attack_speed)
 prime_tearing_arrow = Spell(Vision('prime_tearing_arrow'), Bind('s+e', None), 0.1, 7, get_attack_speed)
-prime_tearing_arrow_cheat = NoCooldownSpell('prime_tearing_arrow', Bind(None, 'left', hold_bind), 1.35, get_attack_speed)
-prime_ultimate_blasting_gust = Spell(Vision('prime_ultimate_blasting_gust'), Bind(None, 'left'), 1.9, 8, get_attack_speed)
+prime_tearing_arrow_cheat = NoCooldownSpell('prime_tearing_arrow', Bind(None, 'left', hold_bind), 1.25, get_attack_speed)
+prime_ultimate_blasting_gust = Spell(Vision('prime_ultimate_blasting_gust'), Bind(None, 'left', hold_bind), 1.9, 8, get_attack_speed)
 prime_shotgun = Spell(Vision('prime_shotgun'), Bind('space', None), 0.3, 3, get_attack_speed)
-prime_wotw = NoCooldownSpell('prime_wotw', Bind('shift+q', None, hold_bind), 0.4, get_attack_speed)
+wotw = Spell(Vision('wotw'), Bind('shift+q', None), 0.4, get_attack_speed)
 e_buff = Spell(Vision('e_buff'), Bind('shift+e', None), 0.9, 180, get_attack_speed)
+prime_hop = NoCooldownSpell('prime_hop', Bind('w', 'right'), 0.7, get_attack_speed)
 
 # Combos
-combo_dp_debuff = Combo([prime_natures_tremble, prime_wotw])
 combo_part_1 = Combo([prime_razor_wind, prime_penetrating_wind, flow_bypassing_wind, prime_shotgun, magnus])
-combo_part_2 = Combo([prime_regeneration, prime_tearing_arrow, prime_tearing_arrow_cheat])
+combo_part_2 = Combo([prime_regeneration, prime_tearing_arrow, prime_tearing_arrow_cheat, prime_hop])
 combo_part_3 = Combo([squall_shot, prime_ultimate_blasting_gust])
 
 
@@ -68,8 +71,10 @@ def shai_buff_active():
 def pve(context):
     if e_buff.ready():
         e_buff.cast(context)
-    elif combo_dp_debuff.ready():
-        combo_dp_debuff.cast(context)
+    elif wotw.ready():
+        wotw.cast(context)
+    elif prime_natures_tremble.ready() and time.time() - prime_natures_tremble.shared_data.last_cast >= 10:
+        prime_natures_tremble.cast(context)
     elif combo_part_1.ready():
         combo_part_1.cast(context)
     elif combo_part_2.ready():
