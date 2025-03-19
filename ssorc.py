5924f92b1bac9acbf89a357a5859d8d81c9402cb
Philippe <philippegarneau1@gmail.com>
2025-03-19 11:14:43 -0400

    added button4

--- ssorc.py
+++ ssorc.py
@@ -26,6 +26,7 @@ def iframe_forward(context):
 listener = Listener(debug=False)
 listener.register_keybind('f24', main)
 listener.register_mouse_override('middle', iframe_forward)
+listener.register_mouse_override('button4', iframe_forward)
 # listener.register_keybind('shift+d', reposition_right, override=True)
 # listener.register_keybind('shift+a', reposition_left, override=True)
 # listener.register_keybind('f22', iframe_forward)
